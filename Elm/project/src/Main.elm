24d486eff99d150bf6900a36643146b319a2c404
武満世阿弥 (Willard-Southward, Brien) <140352869+geistmond@users.noreply.github.com>
2024-04-05 20:45:23 -0700

    Add files via upload

--- Elm/project/src/Main.elm
+++ Elm/project/src/Main.elm
@@ -1,36 +1,84 @@
+-- Input a user name and password. Make sure the password matches.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/forms.html
+--
+
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
-import Json.Decode exposing (int)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
 
 -- MAIN
 
-main = 
-    Browser.sandbox { init = init, update = update, view = view }
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
 
 
 -- MODEL
 
-type alias Model = Int 
+
+type alias Model =
+  { name : String
+  , password : String
+  , passwordAgain : String
+  }
+
 
 init : Model
-init = 
-    0
+init =
+  Model "" "" ""
+
+
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+
+type Msg
+  = Name String
+  | Password String
+  | PasswordAgain String
+
 
 update : Msg -> Model -> Model
 update msg model =
-    case msg of 
-    Increment -> 
-        model + 1
-    
-    Decrement ->
-        model - 1
-    
+  case msg of
+    Name name ->
+      { model | name = name }
+
+    Password password ->
+      { model | password = password }
+
+    PasswordAgain password ->
+      { model | passwordAgain = password }
+
+
+
 -- VIEW
 
-view : Model -> Html Msg
 
+view : Model -> Html Msg
+view model =
+  div []
+    [ viewInput "text" "Name" model.name Name
+    , viewInput "password" "Password" model.password Password
+    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+    , viewValidation model
+    ]
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+  input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+  if model.password == model.passwordAgain then
+    div [ style "color" "green" ] [ text "OK" ]
+  else
+    div [ style "color" "red" ] [ text "Passwords do not match!" ]
