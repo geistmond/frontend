826acdfdfd94001e509b5400f652e8449cc0522f
武満世阿弥 (Willard-Southward, Brien) <140352869+geistmond@users.noreply.github.com>
2024-04-05 20:43:20 -0700

    Add files via upload

---
+++ Elm/project/src/Main.elm
@@ -0,0 +1,36 @@
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Json.Decode exposing (int)
+
+-- MAIN
+
+main = 
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+-- MODEL
+
+type alias Model = Int 
+
+init : Model
+init = 
+    0
+
+-- UPDATE
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of 
+    Increment -> 
+        model + 1
+    
+    Decrement ->
+        model - 1
+    
+-- VIEW
+
+view : Model -> Html Msg
+
